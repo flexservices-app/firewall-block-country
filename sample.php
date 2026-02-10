a9732b46603befe96fb91d6a69686833cc1adc31
flexservices-app <info@flexservices.app>
2026-02-10 20:53:37 +0100

    Create sample.php

---
+++ sample.php
@@ -0,0 +1,17 @@
+How to block an IP from China
+
+$app = new COM("FlexFirewallManager.Admin");
+$app->FormatOutput="json";
+$app->Username="Plesk Domain User";
+$app->Password="Plesk Domain Pass";
+$strIP = $_SERVER['HTTP_X_FORWARDED_FOR'];
+if ($strIP == ""){$strIP = $_SERVER['REMOTE_ADDR'];}
+
+$returnstring = app->InfoIP($strIP);
+
+$appjson = json_decode($returnstring);
+if($appjson->result){
+  if ($appjson->countrycode" == "CN"){
+    $app->BlockIp($strIP);
+  }
+}
